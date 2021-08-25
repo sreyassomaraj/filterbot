369ac3667ae46dddfc6f1b0bd9d7d269c1ffdb63
sreyassomaraj <88197685+sreyassomaraj@users.noreply.github.com>
2021-08-25 20:44:37 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = @coolmoviez123,
+                caption = f" {filename} \n @coolmoviez123",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
